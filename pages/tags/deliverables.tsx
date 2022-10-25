295efe559e14875fd7564434af4f202f5d83cd02
montamu <kimremon0611@gmail.com>
2022-10-25 14:19:34 +0900

    first commit

---
+++ pages/tags/deliverables.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Link from 'next/link';
+
+const Deliverables: NextPage = () => {
+  return (
+    <>
+      <h1>deliverables</h1>
+      
+    </>
+  )  
+}
+
+export default Deliverables
---
+++ pages/tags/skills.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Link from 'next/link';
+
+const Skills: NextPage = () => {
+  return (
+    <>
+      <h1>skills</h1>
+      
+    </>
+  )  
+}
+
+export default Skills
